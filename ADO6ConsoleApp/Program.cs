cf781543a33fb89b55a095ebacfcca36b2a0b49e
Sinazo Ndovela <sinazodia95@gmail.com>
2022-06-08 14:09:36 +0200

    added facilitator feature for added when member 'facilitator' is inserted

--- ADO6ConsoleApp/Program.cs
+++ ADO6ConsoleApp/Program.cs
@@ -11,11 +11,17 @@ namespace AD06ConsoleApp // Note: actual namespace depends on the project name.
         {
             //this is a connection string which is used to store the details.
             string connect = "Server=LAPTOP-L4RD8ILN; Database=AppFactoryDB;Integrated Security = True;";
-            string name, Lname, Gender;
+            string name, Lname, Gender, Qaulif;
             string IdNU;
+            string position;
+            int experience;
+            DateTime starts, ends;
+
+            
             if (!Console.IsInputRedirected)
             {
                 Console.WriteLine("Please Enter The Fields Required");
+                
                 Console.WriteLine("Please Enter Your Name");
                 name = Console.ReadLine();
                 Console.WriteLine("Please Enter Your Last Name");
@@ -23,8 +29,45 @@ namespace AD06ConsoleApp // Note: actual namespace depends on the project name.
                 Console.WriteLine("Please Enter Your ID number");
                 IdNU= Console.ReadLine();
                 Console.WriteLine("Please Enter Your Gender");
-               Gender= Console.ReadLine();
-                Access.AddMember(name, Lname, IdNU, Gender, connect);
+                Gender= Console.ReadLine();
+
+                Console.WriteLine("Please Enter you Postion");
+             
+                position = Console.ReadLine();
+                
+               
+
+                if (position == "facilitator")
+                {
+
+                    Console.WriteLine("Please Enter the qualification");
+                    Qaulif= Console.ReadLine();
+                    Console.WriteLine("please enter your postion");
+                    position = Console.ReadLine();
+                    Console.WriteLine("Please Enter years of experience");
+                    experience = Convert.ToInt32(Console.ReadLine());
+
+                    Console.Write("Enter the month for start: ");
+                    int month = int.Parse(Console.ReadLine());
+                    Console.Write("Enter The day: ");
+                    int day = int.Parse(Console.ReadLine());
+                    Console.Write("Enter The year: ");
+                    int year = int.Parse(Console.ReadLine());
+
+                    starts = new DateTime(year, month, day);
+                  
+                    Console.Write("Enter a month for End: ");
+                    int month1 = int.Parse(Console.ReadLine());
+                    Console.Write("Enter a day: ");
+                    int day1 = int.Parse(Console.ReadLine());
+                    Console.Write("Enter a year: ");
+                    int year1 = int.Parse(Console.ReadLine());
+                    ends = new DateTime(year, month, day);
+
+                    Access.AddFacilitator(Qaulif, position, experience, starts, ends, connect);
+                }
+
+                 Access.AddMember(name, Lname, IdNU, Gender,position, connect);
             }
 
             //access the function from the access layer
--- AccessLayer/Access.cs
+++ AccessLayer/Access.cs
@@ -9,59 +9,46 @@ namespace AccessLayer
     public class Access
     {
        
-        // method grabs string input
-        //create a method that would connect to the database
-        //public static void Connect(string connectionString)
-        //{
-        //    //create connection
-        //    SqlConnection con = new SqlConnection(connectionString);
-        //    //create query that writes to database
-        //    string sql = "Insert Into Member(Member ID, FirstName, LastName, ID Number, Gender) values (@id,'@first_name', '@last_name', '@idNumber', '@gender'  )";
+        public static void AddFacilitator(string qua, string pos, int exp, DateTime start, DateTime end, string connectionString)
+        {
+            var RowsAffected = 0;
 
-        //    //open connection
-        //    con.Open();
-
-        //    //create a sql command referencing the connection
-        //    SqlCommand cmd = new SqlCommand(sql, con);
-        //    cmd.ExecuteNonQuery();
-
-        //    //close connection
-        //    con.Close();
-
-        //    //open connection
-        //    con.Open();
-        //    //create query that reads from database
-        //    string sql2 = "SELECT * FROM YourTable";
-        //    //create a sql command referencing the connection
-        //    SqlCommand cmd2 = new SqlCommand(sql2, con);
-
-        //    //create sql data reader
-        //    SqlDataReader dataReader = cmd2.ExecuteReader();
-
-        //    //while its reading
-        //    while (dataReader.Read())
-        //    {
-        //        //store the columns in variables
-        //        var id = dataReader.GetInt32(0);
-        //        var itemName = dataReader.GetString(1);
-        //        decimal itemPrice = dataReader.GetDecimal(2);
-
-        //        //write to console
-        //        Console.WriteLine(id + " " + itemName + " " + itemPrice);
+            {
+                // Create SQL connection object in using block for automatic closing and disposing
+                using (SqlConnection cnn = new SqlConnection(connectionString))
+                {
+                    // Create command object in using block for automatic disposal
+                    using (SqlCommand cmd = new SqlCommand("Masterinsertfas", cnn))
+                    {
 
 
-        //    }
-        //    //close connection
-        //    con.Close();
-        //    //dispose of connection
-        //    con.Dispose();
+
+                        cmd.Parameters.Add(new SqlParameter("@Qualif", qua));
+                        cmd.Parameters.Add(new SqlParameter("@Position", pos));
+                        cmd.Parameters.Add(new SqlParameter("@experience", exp));
+                        cmd.Parameters.Add(new SqlParameter("@Start", start));
+                        cmd.Parameters.Add(new SqlParameter("@end", end));
+                        cmd.Parameters.Add(new SqlParameter("@StatementType", "insert"));
+
+                        // Set CommandType
+                        cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                        // Open the connection
+                        cnn.Open();
+
+                        // Execute the INSERT statement
+                        cmd.ExecuteNonQuery();
 
 
 
 
-        //}
+                        var ResultText = "Rows Affected: " + RowsAffected.ToString();
+                    }
+                }
 
-        public static void AddMember( string Fname,string Lname, string IdNum, string gender, string connectionString)
+            }
+        }
+
+        public static void AddMember( string Fname,string Lname, string IdNum, string gender, string pos, string connectionString)
         {
             //SqlConnection con = new SqlConnection();
             var RowsAffected = 0;
@@ -82,11 +69,35 @@ namespace AccessLayer
                     {
 
                         // Create input parameters
-                      
+                        string Qauli = "";
+
+                        int fk = 0, exp=0;
+                        var end = new DateTime(2018, 1, 5, 20, 0, 0);
+                        var start = new DateTime(2018, 3, 2, 15, 0, 0);
+                        
+                        if (pos.ToLower() == "software development")
+                        {
+                           fk = 1;
+                        }else if (pos.ToLower() == "digital animation")
+                        {
+                            fk = 2;
+                        }
+                        else if (pos.ToLower() == "cyber security")
+                        {
+                            fk = 3;
+                        }
+                        else if(pos.ToLower()== "Facilitator")
+                        {
+                            fk = 4;
+                            AddFacilitator(Qauli, pos, exp, start,end, connectionString);
+                        }
+
+
                         cmd.Parameters.Add(new SqlParameter("@first_name", Fname));
                         cmd.Parameters.Add(new SqlParameter("@last_name", Lname));
                         cmd.Parameters.Add(new SqlParameter("@idNumber",IdNum));
                         cmd.Parameters.Add(new SqlParameter("@gender", gender));
+                        cmd.Parameters.Add(new SqlParameter("@pos", fk));
                         cmd.Parameters.Add(new SqlParameter("@StatementType", "insert"));
 
                         // Set CommandType
@@ -96,8 +107,11 @@ namespace AccessLayer
 
                         // Execute the INSERT statement
                          cmd.ExecuteNonQuery();
+                        
 
-                         ResultText = "Rows Affected: " + RowsAffected.ToString();
+                        
+
+                         ResultText =$"Rows Affected: " + RowsAffected.ToString();
                     }
                 }
             }
